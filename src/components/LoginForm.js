27dab82fc5e24e207a52f692803a0ec7b21c2c7a
Mitesh21jun <msahu21jun@gmail.com>
2021-09-08 17:05:17 +0530

    == to ===

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link,Router } from "react-router-dom";
+import { Link, Router } from "react-router-dom";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
@@ -21,7 +21,7 @@ const LoginForm = () => {
         if (!user) {
           submitForm(e)
         }
-        if (password == user.password) {
+        if (password === user.password) {
           console.log("success")
         }
         return (<Link to={"/user-welcome"} username={username} />)
